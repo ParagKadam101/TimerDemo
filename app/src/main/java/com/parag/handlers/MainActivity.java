dc5eb64df1e6142cccefb678e5f64ac18d404f41
Parag Kadam <parag.kadam@indiacom.com>
2017-08-12 20:34:13 +0530

    First commit

--- app/src/main/java/com/parag/handlers/MainActivity.java
+++ app/src/main/java/com/parag/handlers/MainActivity.java
@@ -1,12 +1,11 @@
 package com.parag.handlers;
 
-import android.os.Handler;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
